c8dff074a3fd698ea5c003059492ef5a8cd087de
Theo <kkw.theo@gmail.com>
2025-03-18 15:04:57 +0900

    feat: 네이게이션 바 컴포넌트 생성하기

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+export const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log(window.scrollY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        src="//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        src="https://occ-0-2218-325.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABfjwXqIYd3kCEU6KWsiHSHvkft8VhZg0yyD50a_pHXku4dz9VgxWwfA2ontwogStpj1NE9NJMt7sCpSKFEY2zmgqqQfcw1FMWwB9.png?r=229"
+        alt="User logged"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+};
