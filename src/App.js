addad094466c4c74f0d189aad18249d7e6cb75d3
Theo <kkw.theo@gmail.com>
2025-03-18 13:40:03 +0900

    feat: netflix-clone-project init

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
-import React, { useCallback, useEffect, useState } from "react";
-import "./App.css";
-import List from "./components/List";
-import Form from "./components/Form";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-  console.log("App is Rendering");
-  const [todoData, setTodoData] = useState([]);
-  const [value, setValue] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    let newTodo = {
-      id: Date.now(),
-      title: value,
-      completed: false,
-    };
-    setTodoData((prev) => [...prev, newTodo]);
-    setValue("");
-    localStorage.setItem("todoData", JSON.stringify([...todoData, newTodo]));
-  };
-
-  const handleClick = useCallback(
-    (id) => {
-      const newTodoData = todoData.filter((todo) => todo.id !== id);
-      setTodoData(newTodoData);
-      localStorage.setItem("todoData", JSON.stringify(newTodoData));
-    },
-    [todoData]
-  );
-
-  const handleRemoveClick = () => {
-    setTodoData([]);
-  };
-
-  useEffect(() => {
-    const initialTodoData = localStorage.getItem("todoData");
-
-    try {
-      setTodoData(initialTodoData ? JSON.parse(initialTodoData) : []);
-    } catch (e) {
-      console.error(e);
-    }
-  }, []);
-
+function App() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
-      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
-        <div className="flex justify-between mb-3">
-          <h1>할 일 목록</h1>
-          <button onClick={handleRemoveClick}>Delete All</button>
-        </div>
-        <List
-          todoData={todoData}
-          setTodoData={setTodoData}
-          handleClick={handleClick}
-        />
-        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
