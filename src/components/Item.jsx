91dfe67b9b7a52b2b518811d25b4d8712e7df0c9
Theo <kkw.theo@gmail.com>
2025-03-18 12:10:30 +0900

    feat: edit mode

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({
   provided,
@@ -10,7 +10,9 @@ function Item({
   id,
   handleClick,
 }) {
-  console.log("Item is Rendering");
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+
   const handleCompleteChange = (id) => {
     const newTodoData = todoData.map((data) => {
       if (data.id === id) {
@@ -22,13 +24,59 @@ function Item({
     setTodoData(newTodoData);
   };
 
+  const handleEditChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const newTodoData = todoData.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, title: editedTitle };
+      }
+      return todo;
+    });
+    setTodoData(newTodoData);
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <div className="flex items-center justify-between w-full px-4 bg-gray-100">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+            type="text"
+            value={editedTitle}
+            autoFocus
+            onChange={handleEditChange}
+          />
+        </form>
+        <div className="items-center">
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => setIsEditing(false)}
+          >
+            X
+          </button>
+          <button
+            onClick={handleSubmit}
+            className="px-4 py-2 float-right"
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div
       {...provided.draggableProps}
       ref={provided.innerRef}
       {...provided.dragHandleProps}
       className={`${
-        snapshot.isDragging ? "bg-gray-400" : "bg-gray-300"
+        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
       } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
     >
       <div className="items-center">
@@ -39,7 +87,8 @@ function Item({
         />
         <span className={completed ? "line-through" : ""}> {title}</span>
       </div>
-      <div className="items-center">
+      <div className="flex items-center">
+        <button onClick={() => setIsEditing(true)}>Edit</button>
         <button
           className="px-4 py-2 float-right"
           onClick={() => handleClick(id)}
