43afc2aa79a52702f7b7af9b7ef2aa6d8ce83987
Theo <kkw.theo@gmail.com>
2025-03-19 14:39:43 +0900

    chore: gitignore

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "672c938b7ab885955f41457b5c5f6b76",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
