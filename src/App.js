1eb7f901e4c2a22c5352ef946827e1db996263a5
Theo <kkw.theo@gmail.com>
2025-03-19 15:46:37 +0900

    feat: testing-app init

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import { Outlet, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
-import MainPage from "./pages/MainPage";
-import SearchPage from "./pages/SearchPage";
-import DetailPage from "./pages/DetailPage";
-
-const Layout = () => {
-  return (
-    <div>
-      <Nav />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-};
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/:movieId" element={<DetailPage />} />
-        </Route>
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
