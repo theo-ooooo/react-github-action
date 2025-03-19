947875502360a42996775fe1bc0513eb4d991c7e
Theo <kkw.theo@gmail.com>
2025-03-19 12:40:26 +0900

    feat: 검색 페이지

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -16,6 +19,12 @@ const Nav = () => {
       window.removeEventListener("scroll", () => {});
     };
   }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
@@ -23,6 +32,11 @@ const Nav = () => {
         alt="Netflix Logo"
         className="nav__logo"
         onClick={() => window.location.reload()}
+      />
+      <input
+        className="nav__input"
+        value={searchValue}
+        onChange={handleChange}
       />
       <img
         src="https://occ-0-2218-325.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABfjwXqIYd3kCEU6KWsiHSHvkft8VhZg0yyD50a_pHXku4dz9VgxWwfA2ontwogStpj1NE9NJMt7sCpSKFEY2zmgqqQfcw1FMWwB9.png?r=229"
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import axios from "../../api/axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./SearchPage.css";
 
 function SearchPage() {
-  return <div>SearchPage</div>;
+  const [searchMovies, setSearchMovies] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  const searchTerm = useQuery().get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchMovies(request.data.results);
+    } catch (e) {
+      console.error("error", e);
+    }
+  };
+
+  const renderSearchResults = () => {
+    return searchMovies.length ? (
+      <section className="search-container">
+        {searchMovies.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            return (
+              <div className="movie">
+                <div className="movie__column-poster">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                    alt="movie_poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>Your search for "{searchTerm}" did not have any matches.</p>
+          <p>Suggestions:</p>
+          <ul>
+            <li>Try different keywords</li>
+          </ul>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 }
 
 export default SearchPage;
