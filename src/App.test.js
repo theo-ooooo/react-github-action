d77fed8ceed72c8c3ca3b193c4eddfcb081722b7
Theo <kkw.theo@gmail.com>
2025-03-20 12:41:02 +0900

    feat: counter, plus, minus button 추가

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 
-  const lintTest = screen.getByRole("button", {
-    name: "lineTest",
-  });
+  const counterElement = screen.getByTestId("counter");
 
-  expect(lintTest).toHaveTextContent("lintTest");
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test("minus button has correct text", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
 });
